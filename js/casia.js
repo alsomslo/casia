645d0d0d7e3a31dba60896d22926ea1168341714
alsomslo <133613694+alsomslo@users.noreply.github.com>
2023-10-04 15:10:47 +0900

    Add files via upload

--- js/casia.js
+++ js/casia.js
@@ -1,29 +1,42 @@
 $(function () {
 
+
+    $('.mopen').on('click', function () {
+        $(this).toggleClass('on');
+        $('.gnb').toggleClass('on');
+    });
+
+    $('.gnb').on('wheel touchmove', function (e) {
+        e.preventDefault();
+    });
+
+
     $(window).on('scroll', function () {
         let sct = $(window).scrollTop();
         if (sct > 0) {
-            $('.header').addClass('on')
+            $('.header').addClass('on');
         } else {
-            $('.header').removeClass('on')
+            $('.header').removeClass('on');
         }
-
     });
 
-    $('.mainslide').slick({
+
+    $('.mainSlide').slick({
+        arrows: false,
         fade: true,
         dots: true,
-        arrows: false,
-        autoplay: 3000,
-    })
-
-    $('.mainvisual .arrows .left').on('click', function () {
-        $('.mainslide').slick('slickPrev')
-    });
-    $('.mainvisual .arrows .right').on('click', function () {
-        $('.mainslide').slick('slickNext')
     });
 
 
+    $('.mainVisual .arrows .left').on('click', function () {
+        $('.mainSlide').slick('slickPrev');
+    });
+    $('.mainVisual .arrows .right').on('click', function () {
+        $('.mainSlide').slick('slickNext');
+    });
 
-});
+
+
+
+
+})
